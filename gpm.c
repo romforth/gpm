d20e53b225b28e70caf1da030a826c02a918b425
Rom Embedded Forth <romforth@proton.me>
2026-03-30 10:58:42 -0700

    Add support for '$[0-9]' variables in the macro body

--- gpm.c
+++ gpm.c
@@ -16,6 +16,57 @@ match(char close, char *s) {
 
 char *defs[26];
 
+void
+subst(char *p, int arg, char **args) {
+	char c;
+
+	while (c=*p++) {
+		if (c=='$') {
+			int n=*p++-'0';
+			subst(args[n], arg, args);
+		} else {
+			putchar(c);
+		}
+	}
+}
+
+int
+split(char *buf, int arg, char **args) {
+	char c;
+
+	args[arg++]=buf;
+	while (c=*buf) {
+		if (c==' ') {
+			*buf++=0;
+			args[arg++]=buf;
+		} else {
+			buf++;
+		}
+	}
+	return arg;
+}
+
+void
+expand(char *buf) {
+	char *args[100];
+
+	int arg=split(buf, 0, args);
+
+	if (strcmp(args[0], "def")) {
+		char *body;
+		if (body=defs[*args[0]-'A']) {
+			subst(body, arg, args);
+		} else {
+			for (int i=0;i<arg;i++) {
+				subst(args[i], arg, args);
+				if (i!=arg-1) putchar(' ');
+			}
+		}
+	} else {
+		defs[*args[1]-'A']=strdup(args[2]);
+	}
+}
+
 int
 main() {
 	char c;
@@ -24,16 +75,7 @@ main() {
 	while ((c=getchar()) != EOF) {
 		if (c=='{') {
 			match('}', buf);
-			if (strncmp(buf, "def ", 4)) {
-				char *body;
-				if (body=defs[buf[0]-'A']) {
-					printf("%s",body);
-				} else {
-					printf("%s",buf);
-				}
-			} else {
-				defs[buf[4]-'A']=strdup(buf+6);
-			}
+			expand(buf);
 		} else {
 			putchar(c);
 		}
--- makefile
+++ makefile
@@ -4,5 +4,7 @@ all : gpm test/inp1
 	cmp test/out1 test/exp1
 	./gpm < test/inp2 > test/out2
 	cmp test/out2 test/exp2
+	./gpm < test/inp3 > test/out3
+	cmp test/out3 test/exp3
 clean :
-	rm -f gpm test/out1 test/out2
+	rm -f gpm test/out1 test/out2 test/out3
