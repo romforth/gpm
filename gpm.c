0b4307d2be4257c1f4c0cdaf4d8f9bcf7c9a0531
Rom Embedded Forth <romforth@proton.me>
2026-03-30 10:16:22 -0700

    Add single character macro definitions

--- gpm.c
+++ gpm.c
@@ -1,22 +1,39 @@
 #include <stdio.h>	// getchar, putchar, EOF
+#include <string.h>	// strcmp, strdup
 
 void
-match(char close) {
+match(char close, char *s) {
 	char c;
 
 	while ((c=getchar()) != EOF) {
-		if (c==close) return;
-		putchar(c);
+		if (c==close) {
+			*s++=0;
+			return;
+		}
+		*s++=c;
 	}
 }
+
+char *defs[26];
 
 int
 main() {
 	char c;
+	char buf[100];
 
 	while ((c=getchar()) != EOF) {
 		if (c=='{') {
-			match('}');
+			match('}', buf);
+			if (strncmp(buf, "def ", 4)) {
+				char *body;
+				if (body=defs[buf[0]-'A']) {
+					printf("%s",body);
+				} else {
+					printf("%s",buf);
+				}
+			} else {
+				defs[buf[4]-'A']=strdup(buf+6);
+			}
 		} else {
 			putchar(c);
 		}
--- makefile
+++ makefile
@@ -1,5 +1,8 @@
+LDFLAGS=-g
 all : gpm test/inp1
 	./gpm < test/inp1 > test/out1
 	cmp test/out1 test/exp1
+	./gpm < test/inp2 > test/out2
+	cmp test/out2 test/exp2
 clean :
-	rm -f gpm test/out1
+	rm -f gpm test/out1 test/out2
